90943b1ac65077577544fde61f32671f871f96b0
preitinger <peter.reitinger@gmail.com>
2023-12-15 19:06:29 +0100

    try manifest for offline webapp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+})
+
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = withPWA(nextConfig)
