f6b4915b6ebfe743044eaec7e3691f803b44dca3
preitinger <peter.reitinger@gmail.com>
2023-12-26 09:29:25 +0100

    follow https://blog.stackademic.com/adding-pwa-to-next-js-with-app-directory-simple-guide-73284be7b7c1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
-/** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
+// const withPWA = require('next-pwa')({
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+// })
+
+const withPWA = require("@ducanh2912/next-pwa").default({
     dest: "public",
-    register: true,
-    skipWaiting: true,
-})
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+});
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {}
+
+module.exports = withPWA(nextConfig);
 
 // module.exports = nextConfig
 
-module.exports = process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig)
+// module.exports = process.env.NODE_ENV === 'development' ? nextConfig : withPWA(nextConfig)
 // module.exports = withPWA(nextConfig)
 // module.exports = nextConfig;
